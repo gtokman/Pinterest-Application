646e4642a53959a94295352d1b803cc3d9a3ea78
Gary Guerman Tokman <gtokman@fullsail.edu>
2017-11-24 18:37:41 -0500

    Initial Commit

---
+++ PinterestApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PinterestApplication
+//
+//  Created by Gary Tokman on 11/24/17.
+//  Copyright © 2017 Gary Tokman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
