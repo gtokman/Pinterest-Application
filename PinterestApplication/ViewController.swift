c6cc200ed91148d32b32d95ca96d122cda968645
Gary Guerman Tokman <gtokman@fullsail.edu>
2017-11-24 20:30:34 -0500

    part 1 - build out UI

--- PinterestApplication/ViewController.swift
+++ PinterestApplication/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,19 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+// MARK: Data source
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        
+        return cell 
+    }
 }
 
